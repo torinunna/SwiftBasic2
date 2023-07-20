ba4229242179777a78c36a145c5d5479a2517c48
torinunna <torinunna9588@gmail.com>
2023-07-20 16:36:32 +0900

    Datatype

---
+++ Pages/Datatype.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+// Int: 64bit 정수형
+var someInt: Int = -100
+someInt = 100
+
+// UInt: 부호가 없는 64bit 정수형
+var someUInt: UInt = 200
+
+// Float: 32bit 부동 소수점
+var someFloat: Float = 1.1
+someFloat = 1
+print(someFloat)
+
+// Double: 64bit 부동 소수점
+var someDouble: Double = 2.12
+someDouble = 2
+print(someDouble)
+
+// Bool: true/false 값
+var someBool: Bool = true
+someBool = false
+
+// Character: 문자
+var someCharacter: Character = "가"
+someCharacter = "a"
+someCharacter = "😣"
+
+// String: 문자열
+var someString: String = "안녕"
+
+// Any: 모든 타입
+
+// Type Inference(타입 추론)
+// Type 생략
+var number = 10
